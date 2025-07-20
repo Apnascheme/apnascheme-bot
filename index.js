74a9bd0713f3f244b4946ad82b27a11b0e62d863
Apnascheme <apnascheme.in@gmail.com>
2025-07-20 16:51:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.post('/webhook', async (req, res) => {
 // Start server
 app.listen(PORT, () => {
   console.log(` ApnaScheme bot server started on port ${PORT}`);
-  console.log(==> Available at your primary URL https://apnascheme-bot.onrender.com);
+  console.log("Available at your primary URL https://apnascheme-bot.onrender.com");
   console.log('///////////////////////////////////////////////////////////\n');
 });
