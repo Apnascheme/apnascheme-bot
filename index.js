4f3375218a40dcd3b47e50db3ed615b9ac3566dc
Apnascheme <apnascheme.in@gmail.com>
2025-07-31 11:18:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -314,21 +314,21 @@ app.post('/gupshup', async (req, res) => {
                       + ` सिर्फ ₹49 में पाएं:\n`
                       + `  आपके लिए सभी योजनाओं की पूरी लिस्ट\n`
                       + ` सीधे आवेदन करने के लिंक\n\n`
-                      + ` अभी पेमेंट करें: \nhttps://rzp.io/rzp/razorpay49\n\n`
+                      + ` अभी पेमेंट करें: \nhttps://rzp.io/rzp/apnascheme?notes[phone]=${encodeURIComponent(phone)}\n\n`
                       + ` ऑफर सीमित समय के लिए!`;
     } else if (user.language === '2') {
         closingMessage = ` Amazing News! \nYou're eligible for ${eligibleSchemes.length} government schemes!\n\n`
                       + ` For just ₹49 get:\n`
                       + ` Complete list of all schemes for you\n`
                       + ` Direct application links\n\n`
-                      + ` Make payment now: \nhttps://rzp.io/rzp/razorpay49\n\n`
+                      + ` Make payment now: \nhttps://rzp.io/rzp/apnascheme?notes[phone]=${encodeURIComponent(phone)}\n\n`
                       + `Limited time offer!`;
     } else if (user.language === '3') {
         closingMessage = ` जबरदस्त बातम्या! \nतुम्ही ${eligibleSchemes.length} सरकारी योजनांसाठी पात्र आहात!\n\n`
                       + ` फक्त ₹49 मध्ये मिळवा:\n`
                       + ` तुमच्यासाठी सर्व योजनांची संपूर्ण यादी\n`
                       + ` थेट अर्ज करण्याचे लिंक\n\n`
-                      + ` आत्ताच पेमेंट करा: \nhttps://rzp.io/rzp/razorpay49\n\n`
+                      + ` आत्ताच पेमेंट करा: \nhttps://rzp.io/rzp/apnascheme?notes[phone]=${encodeURIComponent(phone)}\n\n`
                       + ` मर्यादित वेळ ऑफर!`;
     }
 
